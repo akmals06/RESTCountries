a607c0cb1b89d257d170634b78eb559e6d244750
akmals06 <sapiselfie06@gmail.com>
2025-04-08 23:31:29 +0700

    test 2 negara akmal

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error(err));
 
 
-app.use('/api', countryRoutes);
+app.use('/api ', countryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ controllers/countryController.js
@@ -0,0 +1,4 @@
+// controllers/countryController.js
+exports.getAllCountries = (req, res) => {
+    res.send("List of countries");
+  };
