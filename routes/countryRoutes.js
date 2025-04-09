d654e3dd9a8ce1f6c033eeabc7d28e44d8a98a8b
amaziaaa <rafael.535240003@stu.untar.ac.id>
2025-04-10 00:40:03 +0700

    update countryRoutes.js

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -1,1 +1,11 @@
-console.log("testing amazia")
+const express = require('express');
+const { 
+    addCountry, 
+    getAllCountries, 
+    getCountryByName, 
+    getCountryByCode, 
+    getCountriesByRegion, 
+    getCountriesByLanguage 
+} = require('../controllers/countryController');
+
+const router = express.Router();
