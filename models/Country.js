494a4ff911f56c57ca425d10d104ce61d26accee
Perancesco <francescoalexander1234@gmail.com>
2025-04-10 00:54:59 +0700

    Skema sampe symbol

--- models/Country.js
+++ models/Country.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const CountrySchema = new mongoose.Schema({
+    name: {
+        common: { type: String, required: true },
+        official: { type: String, required: true }
+    },
+    code: { type: String, required: true },
+    codes: [String],
+    currency: {
+        code: { type: String, required: true },
+        name: { type: String, required: true },
+        symbol: { type: String, required: true }
+    }
+});
