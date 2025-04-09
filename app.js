db306b1fbefe05cf86ea945ab30cb408c7c9f16e
akmals06 <sapiselfie06@gmail.com>
2025-04-09 10:15:59 +0700

    menambahkan beberapa codingan app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("tes tes 123")
+const express = require('express');
+const mongoose = require('mongoose');
+const countryRoutes = require('./routes/countryRoutes');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+
+mongoose.connect
