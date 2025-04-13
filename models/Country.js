cc54c511fc53a5b3bedd9235ff0eaf8e2330ce0d
Perancesco <francescoalexander1234@gmail.com>
2025-04-13 14:54:38 +0700

    Menyelesaikan body Country.js hingga selesai, dan mengexport module tersebut

--- models/Country.js
+++ models/Country.js
@@ -11,5 +11,14 @@ const CountrySchema = new mongoose.Schema({
         code: { type: String, required: true },
         name: { type: String, required: true },
         symbol: { type: String, required: true }
-    }
+    },
+    demonym: { type: String, required: true },
+    languages: { type: Map, of: String },
+    capital: { type: String, required: true },
+    callingCode: [String],
+    region: { type: String, required: true },
+    subregions: [String],
+    translations: { type: Map, of: String }
+
 });
+module.exports = mongoose.model('Country',CountrySchema);
