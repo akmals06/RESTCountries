062fbf34103d6e68fd6b0bd30aa0688616f14818
amaziaaa <rafael.535240003@stu.untar.ac.id>
2025-04-18 20:52:41 +0700

    ngepush

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -9,4 +9,12 @@ const {
 } = require('../controllers/countryController');
 
 const router = express.Router();
-tes
+
+router.post('/countries', addCountry); // Manually add a country
+router.get('/countries', getAllCountries); // Get all countries
+router.get('/countries/name/:name', getCountryByName); // Get by name
+router.get('/countries/code/:code', getCountryByCode); // Get by country code
+router.get('/countries/region/:region', getCountriesByRegion); // Get by region
+router.get('/countries/language/:lang', getCountriesByLanguage); // Get by language
+
+module.exports = router;
