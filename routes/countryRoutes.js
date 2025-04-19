ad54dc7a45014f1794fc9091b423838a7b745bd1
Perancesco <francescoalexander1234@gmail.com>
2025-04-19 19:55:21 +0700

    Menambahkan routes Fitur GET capital, callingcode, region, subregions, Fitur UPDATE name, code

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -14,9 +14,9 @@ const {
   getCountryCallingCode,
   getCountryRegion,
   getCountrySubregions,
-  
+
   updateCountryName,
-  updateCountryCode,  
+  updateCountryCode,
 } = require('../controllers/countryController');
 
 const router = express.Router();
@@ -33,5 +33,13 @@ router.get('/countries/code/:code/code', getCountryCode);
 router.get('/countries/code/:code/codes', getCountryCodes);
 router.get('/countries/code/:code/demonym', getCountryDemonym);
 router.get('/countries/code/:code/languages', getCountryLanguages);
+router.get('/countries/code/:code/capital', getCountryCapital);
+router.get('/countries/code/:code/callingCode', getCountryCallingCode);
+router.get('/countries/code/:code/region', getCountryRegion);
+router.get('/countries/code/:code/subregions', getCountrySubregions);
+
+// update
+router.put('/countries/code/:code/name', updateCountryName);
+router.put('/countries/code/:code/code', updateCountryCode);
 
 module.exports = router;
