eaa3e13deab2d2a16ab3808bdb419ae1774c02b1
jerricoo <jerriconatanael19@gmail.com>
2025-04-21 12:53:00 +0700

    Menambahkan methode createCountry untuk membuat countrynya, jadi untuk menambhakan country kita pake conts data = req.body jadi nanti kalau mau ambil negara yang udah kita buat, nanti datanya disimpan ke const data, lalu disitu sy menggunakan if else, jadi nanti user bisa ngepost dengan 2 cara, yaitu menyimpan data negara secara sekaligus yg berupa array,dan nantinya akan menggungakan Country.insertMany, kalau misal mau nambahin 1 negara doang dan bukan berupa array akan menggunakan Country.create Jika berhasil akan mengeluarkan error 201, kalau tidak berhasil 500.

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,1 +1,25 @@
-// akan dikerjakan teman kelompok saya
+const Country = require('../models/Country');
+
+// create
+const createCountry = async (req, res) => {
+  try {
+    const data = req.body;
+    let result;
+
+    if (Array.isArray(data)) {
+      result = await Country.insertMany(data);
+    } else {
+      result = await Country.create(data);
+    }
+
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(500).json({ error: 'gagal menambahkan negara' });
+  }
+};
+
+
+
+module.exports = {
+  createCountry
+};
