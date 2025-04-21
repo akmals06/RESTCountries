3c109b9384949d74ff968859910193078fca5130
akmals06 <sapiselfie06@gmail.com>
2025-04-21 13:10:20 +0700

    menambahkan method

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -18,8 +18,44 @@ const createCountry = async (req, res) => {
   }
 };
 
+// read
+const getCountryBy = async (req, res) => {
+    try {
+      const { key, value } = req.params;
+      let countries = [];
+  
+      if (key === 'all') {
+        countries = await Country.find();
+  
+      } else if (key === 'languages') {
+        const all = await Country.find();
+        countries = all.filter(c => {
+          let vals;
+          if (c.languages instanceof Map) {
+            vals = Array.from(c.languages.values());
+          } else {
+            vals = Object.values(c.languages || {});
+          }
+          return vals.includes(value);
+        });
+        
+      } else {
+        countries = await Country.find({ [key]: value });
+      }
+  
+      if (!countries.length) {
+        return res.status(404).json({ message: 'Negara tidak ditemukan' });
+      }
+  
+      res.json(countries);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+    }
+  };
 
 
 module.exports = {
-  createCountry
+  createCountry,
+  getCountryBy,
 };
